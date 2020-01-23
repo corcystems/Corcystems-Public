e91401c53131f2655e049dea91728c50f7a885f7
Corcystems <54687169+corcystems@users.noreply.github.com>
2020-01-23 17:02:06 -0500

    Update batttest.ps1

--- batttest.ps1
+++ batttest.ps1
@@ -18,15 +18,15 @@ try {
 if (Test-Path -Path $OutFile_FullPath) {
     $XMLData = New-Object System.XML.XMLDocument
     $XMLData.Load($OutFile_FullPath)
-	
-	$LastTwoUsageEntries = $XMLData.BatteryReport.RecentUsage.UsageEntry | select -last 2
-	
+
+    $LastTwoUsageEntries = $XMLData.BatteryReport.RecentUsage.UsageEntry | select -last 2
+
     $MostRecent = [int]$LastTwoUsageEntries[1].FullChargeCapacity
-	$NextMostRecent = [int]$LastTwoUsageEntries[0].FullChargeCapacity
-	$CutoffAmount = $NextMostRecent * $PercentDifferce
-​
-	$CutoffAmount
-	if ($MostRecent -lt $CutoffAmount) {
-		Write-Host "Battery Max Charge Fell by a detrimental amount"
-	}
+    $NextMostRecent = [int]$LastTwoUsageEntries[0].FullChargeCapacity
+    $CutoffAmount = $NextMostRecent * $PercentDifferce
+
+​    $CutoffAmount
+ if ($MostRecent -lt $CutoffAmount) {
+    Write-Host "Battery Max Charge Fell by a detrimental amount"
+    }
 }
