08561611a5e4596dd10f76ad3714dbc2ca825957
Corcystems <54687169+corcystems@users.noreply.github.com>
2021-10-11 12:13:40 -0400

    Update speedtest.ps1

--- speedtest.ps1
+++ speedtest.ps1
@@ -1,7 +1,7 @@
 $speedtestURL = "https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-win64.zip"
 $speedtestZip = "C:\Windows\Temp\speedtest.zip"
 $speedtestDir = "C:\Windows\Temp\speedtest\"
-$speedtestExe = [ScriptBlock]::Create("C:\Windows\Temp\speedtest\speedtest.exe --progress=no --format=human-readable --accept-license")
+$speedtestExe = [ScriptBlock]::Create("C:\Windows\Temp\speedtest\speedtest.exe --accept-license --progress=no --format=human-readable")
 
 Invoke-WebRequest -Uri $speedtestURL -Outfile $speedtestZip
 Expand-Archive -LiteralPath $speedtestZip -DestinationPath $speedtestDir -Force
