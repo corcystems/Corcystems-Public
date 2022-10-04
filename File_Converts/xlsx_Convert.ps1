13d0414196f6dab332d2cfcea137adb882825902
Corcystems <54687169+corcystems@users.noreply.github.com>
2022-10-04 12:52:50 -0400

    Update xlsx_Convert.ps1

--- File_Converts/xlsx_Convert.ps1
+++ File_Converts/xlsx_Convert.ps1
@@ -1,61 +1,83 @@
-Function Get-Folder($initialDirectory="")
+# Function to pull the folder path to convert. Includes all subfolders.
+Function Get-Folder($initialDirectory=""){
+	[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
 
-{
-    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+	$foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+	$foldername.Description = "Select a folder"
+	$foldername.rootfolder = "MyComputer"
+	$foldername.SelectedPath = $initialDirectory
 
-    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
-    $foldername.Description = "Select a folder"
-    $foldername.rootfolder = "MyComputer"
-    $foldername.SelectedPath = $initialDirectory
+	if($foldername.ShowDialog() -eq "OK"){
+	$folder += $foldername.SelectedPath
+	}
+	return $folder
+} # End of the Get-Folder function
 
-    if($foldername.ShowDialog() -eq "OK")
-    {
-        $folder += $foldername.SelectedPath
-    }
-    return $folder
-}
-
+# Call the Get-Folder 
 $folderpath = Get-Folder
 
+# Create the Word object
 Add-Type -AssemblyName Microsoft.Office.Interop.Word
 $docFixedFormat = [Microsoft.Office.Interop.Word.WdSaveFormat]::wdFormatXMLDocument
 write-host $docFixedFormat
 $word = New-Object -ComObject Word.Application
 $word.visible = $true
-$filetype ="*xls"
 
+# Sets the file types
+$filetype ="*xls"
+$newFileExt = ".xlsx"
+
+# Grab all files in the path chosen above.
 $allFiles = Get-ChildItem -Path $folderpath -Include $filetype -recurse
 
+# Start of the Convertion Loop.
+# Loops through all files found above.
+# Begin is just setting a counter for the progress bar.
 $allFiles | ForEach-Object -Begin {
 	# Set the $i counter variable to zero.
 	$i = 0
 
-} -Process {
+	# Actual loop that opens each file found matching the extention, saves, closes, then moved the old file.
+	} -Process {
 
-	$i = $i+1
-	$Completed = ($i/$allFiles.count*100)
-	Write-Progress -Activity "Searching Files" -Status "$Completed% Complete:" -PercentComplete $Completed
+		# Raise the counter by 1 for the progress bar.
+		$i = $i+1
+		# Progress bar lines
+		$Completed = ($i/$allFiles.count*100)
+		Write-Progress -Activity "Searching Files" -Status "$Completed% Complete:" -PercentComplete $Completed
 
-	$path = ($_.fullname).substring(0, ($_.FullName).lastindexOf("."))
-    
-	"Converting $path"
-	$document = $word.Documents.open($_.fullname)
+		# Set the path variable.
+		$path = ($_.fullname).substring(0, ($_.FullName).lastindexOf("."))
+	
+		# Converting File
+		"Converting $path"
+		$document = $word.Documents.open($_.fullname)
  
-	$path += ".docx"
-	$document.saveas($path, $docFixedFormat)
-	$document.close()
-    
-	$oldFolder = $path.substring(0, $path.lastIndexOf("\")) + "\old"
-    
-	write-host $oldFolder
-	if(-not (test-path $oldFolder))
-	{
-		new-item $oldFolder -type directory
-	}
+		$path += $newFileExt
+		$document.saveas($path, $docFixedFormat)
+		$document.close()
 
-	move-item $_.fullname $oldFolder
-    
-}
+		# Move the old file to another directory
+		$oldFolder = $path.substring(0, $path.lastIndexOf("\")) + "\old"
+		write-host $oldFolder
+		
+			# Test if the old folder path was already created, if not create it.
+			if(-not (test-path $oldFolder)){
+				new-item $oldFolder -type directory
+			}
+
+		#Grab Last modified Date
+		$fileLastModified = $_.LastWriteTime
+
+		move-item $_.fullname $oldFolder
+
+		#Update Last Modified time on the new file to match the old modified time
+		Get-ChildItem $path | % {$_.LastWriteTime = $fileLastModified}
+	
+
+	} # End of the convertion Loop
+
+# End of script cleanup
 $word.Quit()
 $word = $null
 [gc]::collect()
