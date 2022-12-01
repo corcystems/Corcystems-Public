6a60d79ac39a19f469cd350f086f532e64916da3
Corcystems <54687169+corcystems@users.noreply.github.com>
2022-12-01 12:50:39 -0500

    Update CW_Risk_Assesment_Deploy.ps1

--- RiskAssesments/CW_Risk_Assesment_Deploy.ps1
+++ RiskAssesments/CW_Risk_Assesment_Deploy.ps1
@@ -1,36 +1,27 @@
-#############   Start Script   ################
+if($clientURL -eq $null){
+	Write-host "No URL variable was passed. Exiting script."
+	exit
+}
 
-###############################################
-### CW Risk Assessment Agent Deploy & Run   ###
-### This script was written by Aaron Falzon ###
-### Use of this script is at your own risk  ###
-###############################################
+$clientURLArray = $clientURL.Split("/")
 
-#Get Client Name & Token from the IT Support Portal under: Security > Assessment > (Create Assessment or select client name if already created) > (token is in the link, and starts with 'TKN')
-$clientName = "" ## Note: Place Client Name here as setup in the ITS Portal
-$token = "" ## Note: Place Token here as Displayed in the ITS Portal (starting with TKN)
+$clientName = $clientURLArray[($clientURLArray.count - 2)]
+$token = $clientURLArray[($clientURLArray.count - 1)]
 
-#leave true to remove files after the script has ran, this will also remove the log files from the PC, if you want the logs & files to be left set to $false
-$deleteFile = $true
 
-$url = https://prod.setup.itsupport247.net/downloadassessment/$clientName/$token
+#$url = https://prod.setup.itsupport247.net/downloadassessment/$clientName/$token
 $path = "c:\RiskAssess\"
 $file = "$path\ConnectWise-assessment-utility_$token.exe"
-if (Test-Path -Path $path) {
+if(Test-Path -Path $path){
     "Path exists!"
-} else {
+}else{
     "Path doesn't exist."
     New-Item -Path "c:\" -Name "RiskAssess" -ItemType "directory"
 }
 
-#iwr $url -OutFile $path\temp.tmp
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile($url,"$file")
+$WebClient.DownloadFile($clientURL,"$file")
 
-start-process $file -WorkingDirectory $path -Wait
+Start-Process $file -WorkingDirectory $path -Wait
 
-if ($deleteFile -eq $true){
-    Remove-Item -Force -Recurse -Path "$path"
-}
-
-#############   End Script   ################
+Remove-Item -Force -Recurse -Path "$path"
