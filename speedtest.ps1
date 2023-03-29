372eb5cb205cbb752335b359ff60547218f7c9ed
Corcystems <54687169+corcystems@users.noreply.github.com>
2023-03-29 10:53:00 -0400

    Update speedtest.ps1

--- speedtest.ps1
+++ speedtest.ps1
@@ -1,4 +1,4 @@
-$speedtestURL = "https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-win64.zip"
+$speedtestURL = "https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-win64.zip"
 $speedtestZip = "C:\Windows\Temp\speedtest.zip"
 $speedtestDir = "C:\Windows\Temp\speedtest\"
 $speedtestExe = [ScriptBlock]::Create("C:\Windows\Temp\speedtest\speedtest.exe --accept-license --progress=no --format=human-readable")
