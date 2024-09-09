99250cfb7cfb9081bbddb6b0a6c49275f9cc8bc3
corMHauser <158617946+corMHauser@users.noreply.github.com>
2024-09-09 16:18:40 -0400

    Update Cor_Script.ps1

--- CorTools/Cor_Script.ps1
+++ CorTools/Cor_Script.ps1
@@ -1,2 +1,10 @@
 ## Created by Mike Hauser 
 ## Version 0.1
+
+
+## Get Hash
+$wc = [System.Net.WebClient]::new()
+$pkgurl = 'https://github.com/PowerShell/PowerShell/releases/download/v6.2.4/powershell_6.2.4-1.debian.9_amd64.deb'
+$publishedHash = '8E28E54D601F0751922DE24632C1E716B4684876255CF82304A9B19E89A9CCAC'
+$FileHash = Get-FileHash -InputStream ($wc.OpenRead($pkgurl))
+$FileHash.Hash -eq $publishedHash
