b6457b029b9de90054402814db0fbb4c3ba965e3
corMHauser <158617946+corMHauser@users.noreply.github.com>
2024-09-09 16:21:41 -0400

    Update Cor_Script.ps1

--- CorTools/Cor_Script.ps1
+++ CorTools/Cor_Script.ps1
@@ -4,7 +4,7 @@
 
 ## Get Hash
 $wc = [System.Net.WebClient]::new()
-$pkgurl = 'https://github.com/PowerShell/PowerShell/releases/download/v6.2.4/powershell_6.2.4-1.debian.9_amd64.deb'
-$publishedHash = '8E28E54D601F0751922DE24632C1E716B4684876255CF82304A9B19E89A9CCAC'
+$pkgurl = 'https://raw.githubusercontent.com/corcystems/Corcystems-Public/master/CorTools/Cor_Script.ps1'
+$localHash = Get-FileHash C:\Cor\Cor_Script.ps1 -Algorithm SHA256
 $FileHash = Get-FileHash -InputStream ($wc.OpenRead($pkgurl))
-$FileHash.Hash -eq $publishedHash
+$FileHash.Hash -eq $localHash
