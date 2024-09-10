c3bc61a8080df5162fe0bd07998d2e6d5e5e7b8f
corMHauser <158617946+corMHauser@users.noreply.github.com>
2024-09-10 09:29:31 -0400

    Delete CorTools/Cor_Script.ps1

--- CorTools/Cor_Script.ps1
+++
@@ -1,9 +0,0 @@
-## Created by Mike Hauser 
-## Version 0.1
-
-## Get Hash
-$wc = [System.Net.WebClient]::new()
-$pkgurl = 'https://raw.githubusercontent.com/corcystems/Corcystems-Public/master/CorTools/Cor_Script.ps1'
-$localHash = Get-FileHash C:\Cor\Cor_Script.ps1 -Algorithm SHA256
-$FileHash = Get-FileHash -InputStream ($wc.OpenRead($pkgurl)) -Algorithm SHA256
-$FileHash.Hash -eq $localHash.Hash
